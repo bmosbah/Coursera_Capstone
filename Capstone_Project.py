24a65ec87fd5ac92dc33be8e9fb1ab6146fc3a21
bmosbah <mosbah.brahim@gmail.com>
2020-04-22 22:34:15 +0100

    adds Coursera Capstone Week 01 Github

---
+++ Capstone_Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **! This is mainly for the Applied Data Science Capstone project.**
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
